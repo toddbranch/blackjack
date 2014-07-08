16996605398b115b9536ac31da6f71aa53d3641d
Todd Branchflower <todd.branchflower@gmail.com>
2014-07-08 14:47:15 -0600

    major refactoring - moved the majority of game functionality into Blackjack class, created many helper functions in main to clean up main game logic

--- blackjack.py
+++ blackjack.py
@@ -23,14 +23,55 @@ class Blackjack:
 
     def newGame(self):
         self.gameDeck.shuffle()
+        self.playerHand = []
+        self.dealerHand = []
 
-    def deal(self, playerHand, dealerHand):
+    def deal(self):
         for i in range(2):
-            playerHand.append(self.gameDeck.deal())
-            dealerHand.append(self.gameDeck.deal())
+            self.playerHand.append(self.gameDeck.deal())
+            self.dealerHand.append(self.gameDeck.deal())
 
-    def hit(self, hand):
-        hand.append(self.gameDeck.deal())
+    def hitPlayer(self):
+        self.playerHand.append(self.gameDeck.deal())
+
+    def hitDealer(self):
+        self.dealerHand.append(self.gameDeck.deal())
+
+    def isPlayerBusted(self):
+        return self.isBusted(self.playerHand)
+
+    def isDealerBusted(self):
+        return self.isBusted(self.dealerHand)
+
+    def isDealerComplete(self):
+        return self.evaluateHand(self.dealerHand) >= 17
+
+    def didPlayerBlackjack(self):
+        return self.checkBlackjack(self.playerHand)
+
+    def didDealerBlackjack(self):
+        return self.checkBlackjack(self.dealerHand)
+
+    def handResult(self):
+        dealerScore = self.evaluateHand(self.dealerHand)
+        playerScore = self.evaluateHand(self.playerHand)
+
+        if dealerScore > playerScore:
+            return "dealer_win"
+        elif dealerScore < playerScore:
+            return "player_win"
+        else:
+            return "push"
+
+    def printGame(self, hideDealerCard):
+        print("Player: ")
+        self.printHand(self.playerHand)
+
+        print("Dealer: ")
+        if hideDealerCard:
+            self.printHidden(self.dealerHand)
+        else:
+            self.printHand(self.dealerHand)
 
     @classmethod
     def evaluateHand(cls, hand):
@@ -73,3 +114,20 @@ class Blackjack:
     @staticmethod
     def payBlackjack(bet):
         return bet * 3/2
+
+    @staticmethod
+    def payWin(bet):
+        return bet
+
+
+    @staticmethod
+    def printHand(hand):
+        for card in hand:
+            print(card.prettyPrint(), end="")
+        print()
+
+    @staticmethod
+    def printHidden(hand):
+        print(hand[0].prettyPrint(), end="")
+        print("|X|")
+
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from blackjack import Blackjack
 
-#TODO: Refactor - a lot of this should be moved into the blackjack module, main.py should be primarily for handling user input
-
-# TODO: handle non-integer inputs
 def getPlayerBet(bank):
     bet = bank + 1
 
@@ -15,97 +12,129 @@ def getPlayerBet(bank):
 
     return bet
 
+def getPlayerAction():
+    action = ""
+    while action != 'S' and action != 'H':
+        print("Only (H)it and (S)tand are valid commands.")
+        action = input("(H)it or (S)tand?: ").upper()
+
+    return action
+
+def printGameDealerHidden(game):
+    printDivider()
+    game.printGame(True)
+    printDivider()
+
+def printGameDealerVisible(game):
+    printDivider()
+    game.printGame(False)
+    printDivider()
+
 def printDivider():
-    print("#################################")
-
-def printHand(hand):
-    for card in hand:
-        card.prettyPrint()
-    print()
-
-def printHidden(hand):
-    hand[0].prettyPrint()
-    print("|X|", sep="", end="")
-    print()
-
-def printGame(playerHand, dealerHand, hide):
-    printDivider()
-    print("Player: ")
-    printHand(playerHand)
-    print("Dealer: ")
-    if hide:
-        printHidden(dealerHand)
-    else:
-        printHand(dealerHand)
-    printDivider()
+    print("###########################################")
 
 def printBank(playerBank):
     print("Player Bank:", playerBank)
     printDivider()
 
+def gameOver():
+    print("You lost all your money!")
+    print("Restarting game...")
+    printDivider()
+
+def push(bank, bet):
+    print("PUSH")
+    return bank
+
+def dealer_win(bank, bet):
+    print("DEALER WINS!")
+    return bank - bet
+
+def player_win(bank, bet):
+    print("PLAYER WINS!")
+    return bank + Blackjack.payWin(bet)
+
+game_results = {
+                    "player_win": player_win,
+                    "dealer_win": dealer_win,
+                    "push": push
+        }
+
+def playerBlackjack(bank, bet):
+    print("BLACKJACK!")
+    return bank + Blackjack.payBlackjack(bet)
+
+def dealerBlackjack(bank, bet):
+    print("Dealer Blackjack!")
+    return bank - bet
+
+def dealerBusted(bank, bet):
+    print("DEALER BUSTED!")
+    return bank + bet
+
+def playerBusted(bank, bet):
+    print("PLAYER BUSTED!")
+    return bank - bet
+
+################################################################
+# Game logic
+################################################################
+
 game = Blackjack()
 
 while True:
+
+    ############################################################
+    # Reset game
+    ############################################################
+
     print("Welcome to Blackjack!")
     playerBank = 100.0
 
     while playerBank > 0:
+
+        ########################################################
+        # Play next hand
+        ########################################################
+
         printBank(playerBank)
 
         game.newGame()
 
         playerBet = getPlayerBet(playerBank)
-        playerHand = []
-        dealerHand = []
 
-        game.deal(playerHand, dealerHand)
+        game.deal()
 
-        printGame(playerHand, dealerHand, True)
+        printGameDealerHidden(game)
 
-        if Blackjack.checkBlackjack(playerHand):
-            print("BLACKJACK!")
-            playerBank += Blackjack.payBlackjack(playerBet)
-        elif Blackjack.checkBlackjack(dealerHand):
-            print("Dealer Blackjack!")
-            printGame(playerHand, dealerHand, False)
-            playerBank -= playerBet
+        if game.didPlayerBlackjack():
+            playerBank = playerBlackjack(playerBank, playerBet)
+        elif game.didDealerBlackjack():
+            printGameDealerVisible(game)
+            playerBank = dealerBlackjack(playerBank, playerBet)
         else:
-            while (not Blackjack.isBusted(playerHand)):
-                action = input("Hit (H) or Stand (S)?: ").upper()
+            while not game.isPlayerBusted():
+                action = getPlayerAction()
 
                 if action == 'S':
                     break
-                elif action == 'H':
-                    game.hit(playerHand)
                 else:
-                    print("Only H and S are valid commands.")
+                    game.hitPlayer()
 
-                printGame(playerHand, dealerHand, True)
+                printGameDealerHidden(game)
 
-            if Blackjack.isBusted(playerHand):
-                print("BUSTED!")
-                playerBank -= playerBet
+            if game.isPlayerBusted():
+                playerBank = playerBusted(playerBank, playerBet)
             else:
-                printGame(playerHand, dealerHand, False)
+                printGameDealerVisible(game)
 
-                while(Blackjack.evaluateHand(dealerHand) < 17):
-                    game.hit(dealerHand)
-                    printGame(playerHand, dealerHand, False)
+                while not game.isDealerComplete():
+                    game.hitDealer()
+                    printGameDealerVisible(game)
                     
-                dealerScore = Blackjack.evaluateHand(dealerHand)
-                playerScore = Blackjack.evaluateHand(playerHand)
-
-                if Blackjack.isBusted(dealerHand):
-                    playerBank += playerBet
-                    print("DEALER BUSTED!")
-                elif dealerScore > playerScore:
-                    playerBank -= playerBet
-                    print("DEALER WINS!")
-                elif dealerScore < playerScore:
-                    playerBank += playerBet
-                    print("PLAYER WINS!")
+                if game.isDealerBusted():
+                    playerBank = dealerBusted(playerBank, playerBet)
                 else:
-                    print("PUSH!")
+                    playerBank = game_results[game.handResult()](playerBank, playerBet)            
 
-    print("You lost all your money!")
-    printDivider()
+    gameOver()
