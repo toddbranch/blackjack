ff666e620477218ece38459fd80dff746a7f9a3e
Todd Branchflower <todd.branchflower@gmail.com>
2014-07-08 11:29:04 -0600

    completed basic player gameplay

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import blackjack
 
+# TODO: handle non-integer inputs
 def getPlayerBet(bank):
     bet = bank + 1
 
     while bet > bank:
-        bet = input("Place your bet: ")
+        bet = int(input("Place your bet: "))
 
     return bet
+
+def printDivider():
+    print("#################################")
+
+def printGame(playerHand, dealerHand):
+    printDivider()
+    print("Player: ")
+    for card in playerHand:
+        card.prettyPrint()
+    print()
+    print("Dealer: ")
+    for card in dealerHand:
+        card.prettyPrint()
+    print()
+    printDivider()
+
+def printBank(playerBank):
+    print("Player Bank:", playerBank)
 
 game = blackjack.Blackjack()
 
 while True:
-    print "Welcome to Blackjack!"
+    print("Welcome to Blackjack!")
     playerBank = 100
 
     while playerBank > 0:
+        printBank(playerBank)
+
         game.newGame()
 
         playerBet = getPlayerBet(playerBank)
@@ -22,3 +43,32 @@ while True:
         dealerHand = []
 
         game.deal(playerHand, dealerHand)
+
+        printGame(playerHand, dealerHand)
+
+        if blackjack.Blackjack.checkBlackjack(playerHand):
+            print("BLACKJACK!")
+            playerBank += blackjack.Blackjack.payBlackjack(playerBet)
+        elif blackjack.Blackjack.checkBlackjack(dealerHand):
+            print("Dealer Blackjack!")
+            playerBank -= playerBet
+        else:
+            while (not blackjack.Blackjack.isBusted(playerHand)):
+                action = input("Hit (H) or Stand (S)?: ")
+
+                if action == 'S':
+                    break
+                elif action == 'H':
+                    game.hit(playerHand)
+                else:
+                    print("Only H and S are valid commands.")
+
+                printGame(playerHand, dealerHand)
+
+            if blackjack.Blackjack.isBusted(playerHand):
+                print("BUSTED!")
+                playerBank -= playerBet
+
+
+    print("You lost all your money!")
+    printDivider()
